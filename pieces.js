347d414f5639163b74b73fad3edf12f863a76c13
Dev Unbelievable <devunbelievable@gmail.com>
2023-08-11 06:01:53 +0200

    add stockElement and Description

--- pieces.js
+++ pieces.js
@@ -9,9 +9,16 @@ imageElement.src = article.image;
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
 const prixElement =  document.createElement("p");
-prixElement.innerText = `Prix : ${article.prix} £`;
+//prixElement.innerText = `Prix : ${article.prix} £`;
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorieElement;
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+//operateur nullish ?? pour afficher "(aucune catégorie)" si categorie est null ou undefined
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
+//variable stock
+const stockElement = document.createElement("p");
+stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
 
 //affiche des balises via le parent
 const sectionFiches = document.querySelector('.fiches');
@@ -19,3 +26,5 @@ sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(stockElement);
