65346efb0871c7c90ab7c8ccccba24f66c96fa2c
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-08-23 11:01:15 +0200

    udpate the file avis.js

--- avis.js
+++ avis.js
@@ -4,3 +4,12 @@ function comptLetter(a, n){
 let tourne;
 tourne = comptLetter('just',2);
 console.log(tourne);
+export function ajoutListenersAvis() {
+    const piecesElements = document.querySelectorAll(".fiches article button");
+
+    for (let i = 0; i < piecesElements.length; i++) {
+      piecesElements[i].addEventListener("click", async function (event) {
+           /* ... */
+      });
+    }
+}
