cb1f28acedf3e8e08c07cf25e0fdd219b01120f2
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-08-23 11:26:16 +0200

    add comment on boucle and event

--- avis.js
+++ avis.js
@@ -1,9 +1,9 @@
 //export c'est pour rendre cette function disponible en dehors du fichier
 export function ajoutListenersAvis() {
     const piecesElements = document.querySelectorAll(".fiches article button");
-
-    for (let i = 0; i < piecesElements.length; i++) {
-      piecesElements[i].addEventListener("click", async function (event) {
+    //select all btn in html
+    for (let i = 0; i < piecesElements.length; i++) { //parcourir all btn
+      piecesElements[i].addEventListener("click", async function (event) { //add event
            /* pour récuperer l'id concerne et l'envoyer dans l'url... */
            const id = event.target.dataset.id;
            fetch(`http://localhost:8081/pieces/${id}/avis`);
