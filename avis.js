ac42d0e017e53014e2b1b13f50b3fa3f7393f85e
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-08-26 14:49:59 +0200

    show the avis from API

--- avis.js
+++ avis.js
@@ -5,8 +5,19 @@ export function ajoutListenersAvis() {
     for (let i = 0; i < piecesElements.length; i++) { //parcourir all btn
       piecesElements[i].addEventListener("click", async function (event) { //add event
            /* pour récuperer l'id concerne et l'envoyer dans l'url... */
+           
            const id = event.target.dataset.id;
-           fetch(`http://localhost:8081/pieces/${id}/avis`);
+          const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis"); 
+           //stock la reponse de API
+           const avis = await reponse.json();
+           const pieceElement = event.target.parentElement;
+
+           const avisElement = document.createElement("p");
+            for (let i = 0; i < avis.length; i++) {
+              avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
+
+            }
+            pieceElement.appendChild(avisElement);
       });
     }
 }
